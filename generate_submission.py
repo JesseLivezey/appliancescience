017dc074027599ee2c883b870111341a5e73aa76
JesseLivezey <jesse.livezey@gmail.com>
2013-08-26 17:22:55 -0700

    generating output with H4 apl 2 working, although results are n not correct

--- generate_submission.py
+++ generate_submission.py
@@ -20,25 +20,34 @@ def main(argv):
     with open(eventFile,'rb') as events:
         reader = csv.reader(events,delimiter=',')
         subItems = [item for item in reader]
-    for ii in xrange(10):
-        print subItems[ii]
 
     #Do something with data
-    with open('H4_apID_2_Testing_09_13_1347519601.txt','rb') as first:
+    with open('H4_apID_2_Testing_09_13_1347519601.txt','r') as first:
         reader = csv.reader(first)
-        list1 = [int(round(float(time))) for time in reader]
-    with open('H4_apID_2_Testing_09_18_1347951601.txt','rb') as second:
+        list1 = [int(round(float(time[0]))) for time in reader]
+    with open('H4_apID_2_Testing_09_18_1347951601.txt','r') as second:
         reader = csv.reader(second)
-        list2 = [int(round(float(time))) for time in reader]
-
+        list2 = [int(round(float(time[0]))) for time in reader]
+    print list1[0],list2[0]
+    startLine = -1
+    endLine = -1
     for ii,line in enumerate(subItems):
-        pass
-
+        if startLine == -1 and line[1] == 'H4' and line[2] == '2':
+            startLine = ii
+        elif line[1] == 'H4' and line[2] == '2':
+            endLine = ii+1
+    print startLine,endLine
+    for ii in xrange(startLine,endLine):
+        for jj in xrange(len(list1)):
+            if list1[jj] >= int(round(float(subItems[ii][3]))) and list1[jj] < int(round(float(subItems[ii+1][3]))):
+                subItems[ii][4] = '1'
+            if list2[jj] >= int(round(float(subItems[ii][3]))) and list2[jj] < int(round(float(subItems[ii+1][3]))):
+                subItems[ii][4] = '1'
     if note == '':
         base = 'submission'
     else:
         base = 'submission_'
-    with open(base+note+'.csv','wb') as submission:
+    with open(base+note+'.csv','w+b') as submission:
         writer = csv.writer(submission,delimiter=',')
         for item in subItems:
             writer.writerow(item)
